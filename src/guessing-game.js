99dbc4f0de13d2a24828a9ce8769a1e031c8bb38
LenaYork <lenayork@tut.by>
2019-10-11 21:45:53 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.leftEdge = 0;
+        this.rightEdge = 0;
+        this.userGuess = 0;
+    }
 
     setRange(min, max) {
-
+        this.leftEdge = min;
+        this.rightEdge = max;
     }
 
     guess() {
-
+        return this.userGuess = Math.ceil((this.leftEdge+this.rightEdge)/2);
     }
 
     lower() {
-
+        this.rightEdge = this.userGuess;
     }
 
     greater() {
-
+        this.leftEdge = this.userGuess;
     }
 }
 
